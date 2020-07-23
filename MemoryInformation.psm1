a608b6b82c3df878df9d2b5c6c7379b08997ca92
aetos <aetos@aerie.jp>
2020-07-23 16:09:54 +0900

    platform notice

--- MemoryInformation.psm1
+++ MemoryInformation.psm1
@@ -3,3 +3,7 @@ function Get-PhysicalMemoryInfo {
     CimCmdlets\Get-CimInstance -ClassName Win32_PhysicalMemory -Namespace root\cimv2
 
 }
+
+if (!$global:IsWindows) {
+    Write-Error -Exception ([System.PlatformNotSupportedException]::new('This module supports Windows only.'))
+}
